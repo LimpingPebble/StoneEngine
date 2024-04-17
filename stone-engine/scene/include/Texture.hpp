030e84129bb5e5bfcb1212c560bc761662d3870f
amasson <giantwow3896@gmail.com>
2024-04-17 10:47:44 +0200

    Texture definition

---
+++ stone-engine/scene/include/Texture.hpp
@@ -0,0 +1,30 @@
+// Copyright 2024 Stone-Engine
+
+#pragma once
+
+#include <glm/glm.hpp>
+#include <string>
+
+namespace STN
+{
+
+    class Texture
+    {
+    public:
+        Texture();
+        Texture(const Texture &other);
+
+        virtual ~Texture();
+
+        void setSize(const glm::ivec2 &size);
+        glm::ivec2 getSize() const;
+
+        void setFilePath(const std::string &texture);
+        const std::string &getFilePath() const;
+
+    protected:
+        glm::ivec2 _size;
+        std::string _filePath;
+    };
+
+} // namespace STN
