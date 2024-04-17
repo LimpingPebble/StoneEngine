0ea5e6c45ce85fb54b449e3200d57d8d658eb24a
amasson <giantwow3896@gmail.com>
2024-04-17 16:56:59 +0200

    Node implementation

--- stone-engine/scene/include/Node.hpp
+++ stone-engine/scene/include/Node.hpp
@@ -18,23 +18,46 @@ namespace STN
         class Node : public Object
         {
         public:
-            Node();
+            Node(const std::string &name = "node");
             Node(const Node &other);
 
             virtual ~Node();
 
+            void setName(const std::string &name);
+            const std::string &getName() const;
+            std::string getGlobalName() const;
+
             void addChild(std::shared_ptr<Node> child);
             void removeChild(std::shared_ptr<Node> child);
+            void removeFromParent();
 
             std::shared_ptr<Node> getParent() const;
+            bool hasParent() const;
+            bool isAncestorOf(std::shared_ptr<Node> node) const;
+            bool isDescendantOf(std::shared_ptr<Node> node) const;
             const std::vector<std::shared_ptr<Node>> &getChildren() const;
+            std::shared_ptr<Node> getChild(const std::string &name) const;
+            /**
+             * @brief Get a child node with a path
+             *
+             * @param path The path to the child node. If starting with '*\/', then it will look for any descendant with the given name
+             *
+             * @example getChildWithPath("child1/child2/child3")
+             * @example getChildWithPath("*\/child3")
+             *
+             * @return `std::shared_ptr<Node>` The child node
+             */
+            std::shared_ptr<Node> getChildWithPath(const std::string &path) const;
 
-            virtual void forEachChild(std::function<void(std::shared_ptr<Node>)> callback) const;
+            virtual void forEachChild(std::function<void(std::shared_ptr<Node>)> callback, bool recursively = true) const;
 
             virtual void update(float deltaTime);
-            virtual void render(RenderUniforms &uniforms, RenderStage stage, std::shared_ptr<Scene> scene);
+            virtual void render(const RenderUniforms &uniforms, RenderStage stage, std::shared_ptr<Scene> scene);
+
+            virtual void writeInStream(std::ostream &flux, std::string linePrefix, std::string firstPrefix, std::string lastPrefix) const;
 
         protected:
+            std::string _name;
             std::vector<std::shared_ptr<Node>> _children;
             std::weak_ptr<Node> _parent;
         };
@@ -42,3 +65,21 @@ namespace STN
     } // namespace Scene
 
 } // namespace STN
+
+#define TERM_COLOR_RESET "\033[0m"
+#define TERM_COLOR_BLACK "\033[30m"
+#define TERM_COLOR_RED "\033[31m"
+#define TERM_COLOR_GREEN "\033[32m"
+#define TERM_COLOR_YELLOW "\033[33m"
+#define TERM_COLOR_BLUE "\033[34m"
+#define TERM_COLOR_MAGENTA "\033[35m"
+#define TERM_COLOR_CYAN "\033[36m"
+#define TERM_COLOR_WHITE "\033[37m"
+#define TERM_COLOR_BOLDBLACK "\033[1m\033[30m"
+#define TERM_COLOR_BOLDRED "\033[1m\033[31m"
+#define TERM_COLOR_BOLDGREEN "\033[1m\033[32m"
+#define TERM_COLOR_BOLDYELLOW "\033[1m\033[33m"
+#define TERM_COLOR_BOLDBLUE "\033[1m\033[34m"
+#define TERM_COLOR_BOLDMAGENTA "\033[1m\033[35m"
+#define TERM_COLOR_BOLDCYAN "\033[1m\033[36m"
+#define TERM_COLOR_BOLDWHITE "\033[1m\033[37m"
---
+++ stone-engine/scene/src/Node.cpp
@@ -0,0 +1,214 @@
+// Copyright 2024 Stone-Engine
+
+#include "Node.hpp"
+#include <cassert>
+
+namespace STN
+{
+
+    namespace Scene
+    {
+
+        Node::Node(const std::string &name)
+            : Object(), _name(name), _children(), _parent()
+        {
+            // LOG: Warning: Node name cannot contain '/'
+            assert(name.find('/') == std::string::npos);
+        }
+
+        Node::Node(const Node &other)
+            : Object(other), _name(other._name), _children(other._children), _parent(other._parent)
+        {
+        }
+
+        Node::~Node()
+        {
+        }
+
+        void Node::setName(const std::string &name)
+        {
+            _name = name;
+        }
+
+        const std::string &Node::getName() const
+        {
+            return _name;
+        }
+
+        std::string Node::getGlobalName() const
+        {
+            if (auto parent = getParent())
+            {
+                return parent->getGlobalName() + "/" + getName();
+            }
+            return getName();
+        }
+
+        void Node::addChild(std::shared_ptr<Node> child)
+        {
+            // LOG: Error: Cannot add a parent as a child
+            assert(!child->isAncestorOf(std::dynamic_pointer_cast<Node>(shared_from_this())));
+            child->_parent = std::dynamic_pointer_cast<Node>(shared_from_this());
+            _children.push_back(child);
+        }
+
+        void Node::removeChild(std::shared_ptr<Node> child)
+        {
+            auto it = std::find(_children.begin(), _children.end(), child);
+            if (it != _children.end())
+            {
+                (*it)->_parent.reset();
+                _children.erase(it);
+            }
+        }
+
+        void Node::removeFromParent()
+        {
+            if (auto parent = getParent())
+            {
+                parent->removeChild(std::dynamic_pointer_cast<Node>(shared_from_this()));
+            }
+        }
+
+        std::shared_ptr<Node> Node::getParent() const
+        {
+            return _parent.lock();
+        }
+
+        bool Node::hasParent() const
+        {
+            return !_parent.expired();
+        }
+
+        bool Node::isAncestorOf(std::shared_ptr<Node> node) const
+        {
+            if (auto parent = node->getParent())
+            {
+                if (parent.get() == this)
+                {
+                    return true;
+                }
+                return isAncestorOf(parent);
+            }
+            return false;
+        }
+
+        bool Node::isDescendantOf(std::shared_ptr<Node> node) const
+        {
+            if (auto parent = getParent())
+            {
+                if (parent == node)
+                {
+                    return true;
+                }
+                return parent->isDescendantOf(node);
+            }
+            return false;
+        }
+
+        const std::vector<std::shared_ptr<Node>> &Node::getChildren() const
+        {
+            return _children;
+        }
+
+        std::shared_ptr<Node> Node::getChild(const std::string &name) const
+        {
+            for (auto &child : _children)
+            {
+                if (child->getName() == name)
+                {
+                    return child;
+                }
+            }
+            return nullptr;
+        }
+
+        std::shared_ptr<Node> Node::getChildWithPath(const std::string &path) const
+        {
+            if (path.size() <= 1)
+            {
+                return getChild(path);
+            }
+
+            if (path[0] == '*' && path[1] == '/')
+            {
+                std::string childName = path.substr(2);
+                for (auto &child : _children)
+                {
+                    if (child->getName() == childName)
+                    {
+                        return child;
+                    }
+                    std::shared_ptr<Node> grandChild = child->getChildWithPath(path);
+                    if (grandChild)
+                    {
+                        return grandChild;
+                    }
+                }
+                return nullptr;
+            }
+
+            auto firstSlash = path.find('/');
+
+            if (firstSlash != std::string::npos)
+            {
+                std::string childName = path.substr(0, firstSlash);
+                auto childNode = getChild(childName);
+                if (!childNode)
+                {
+                    return nullptr;
+                }
+                std::string subpath = path.substr(firstSlash + 1);
+                return childNode->getChildWithPath(subpath);
+            }
+
+            return getChild(path);
+        }
+
+        void Node::forEachChild(std::function<void(std::shared_ptr<Node>)> callback, bool recursively) const
+        {
+            for (auto &child : _children)
+            {
+                if (recursively)
+                {
+                    child->forEachChild(callback);
+                }
+                callback(child);
+            }
+        }
+
+        void Node::update(float deltaTime)
+        {
+            for (auto &child : _children)
+            {
+                child->update(deltaTime);
+            }
+        }
+
+        void Node::render(const RenderUniforms &uniforms, RenderStage stage, std::shared_ptr<Scene> scene)
+        {
+            for (auto &child : _children)
+            {
+                child->render(uniforms, stage, scene);
+            }
+        }
+
+        void Node::writeInStream(std::ostream &stream, std::string linePrefix, std::string firstPrefix, std::string lastPrefix) const
+        {
+            stream << linePrefix << firstPrefix << TERM_COLOR_BOLDWHITE << getClassName() << TERM_COLOR_RESET << " (" << TERM_COLOR_BOLDGREEN << _name << TERM_COLOR_RESET << ")" << std::endl;
+            for (size_t i = 0; i < _children.size(); i++)
+            {
+                if (i == _children.size() - 1)
+                {
+                    _children[i]->writeInStream(stream, linePrefix + lastPrefix, "└─", "  ");
+                }
+                else
+                {
+                    _children[i]->writeInStream(stream, linePrefix + lastPrefix, "├─", "│ ");
+                }
+            }
+        }
+
+    } // namespace Scene
+
+} // namespace STN
