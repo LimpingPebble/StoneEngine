c6f87568b98acdc943a5b6f4f2fb9d605a36dabd
amasson <giantwow3896@gmail.com>
2024-04-21 22:28:03 +0200

    Pbr material definitions

--- stone-engine/scene/include/scene/Material.hpp
+++ stone-engine/scene/include/scene/Material.hpp
@@ -27,6 +27,40 @@ namespace Stone
             virtual void generateRenderBehaviour(std::shared_ptr<ISceneRenderer> renderer) override;
         };
 
+        class PBRMaterial : public Material
+        {
+        public:
+            PBRMaterial();
+            PBRMaterial(const PBRMaterial &other);
+
+            virtual ~PBRMaterial();
+
+            virtual const char *getClassName() const override;
+            virtual std::string debugDescription() const override;
+
+            virtual void generateRenderBehaviour(std::shared_ptr<ISceneRenderer> renderer) override;
+
+            std::shared_ptr<Texture> albedo;
+            std::shared_ptr<Texture> normal;
+            std::shared_ptr<Texture> metallic;
+            std::shared_ptr<Texture> roughness;
+            std::shared_ptr<Texture> occlusion;
+        };
+
+        class ShaderMaterial : public Material
+        {
+        public:
+            ShaderMaterial(std::string shader_file);
+            ShaderMaterial(const ShaderMaterial &other);
+
+            virtual ~ShaderMaterial();
+
+            virtual const char *getClassName() const override;
+            virtual std::string debugDescription() const override;
+
+            virtual void generateRenderBehaviour(std::shared_ptr<ISceneRenderer> renderer) override;
+        };
+
     } // namespace Scene
 
 } // namespace Stone
