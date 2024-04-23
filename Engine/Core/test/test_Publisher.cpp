7ffad5fedbb06e0f75e6291efd4cd4bfe48fab7d
amasson <giantwow3896@gmail.com>
2024-04-23 23:24:16 +0200

    Publisher test BulletGame

--- Engine/Core/test/test_Publisher.cpp
+++ Engine/Core/test/test_Publisher.cpp
@@ -100,6 +100,96 @@ TEST(Publisher, SubscribeWithMethodConst)
     EXPECT_EQ(result, 15);
 }
 
+TEST(Publisher, Unbind)
+{
+    int result = 0;
+    auto addition = [&result](int a, int b)
+    {
+        result = a + b;
+    };
+
+    Publisher<void(int, int)> publisher;
+    {
+        Subscriber<void(int, int)> subscriber(addition);
+        publisher.bind(subscriber);
+        publisher.broadcast(5, 8);
+        EXPECT_EQ(result, 13);
+        subscriber.unbind();
+        publisher.broadcast(12, 5);
+        EXPECT_EQ(result, 13);
+    }
+    publisher.broadcast(12, 5);
+    EXPECT_EQ(result, 13);
+}
+
+TEST(Publisher, BulletGame)
+{
+    class Actor
+    {
+    public:
+        Actor() = default;
+        virtual ~Actor() = default;
+    };
+
+    class PhysicBody
+    {
+    public:
+        Publisher<void(Actor *)> onHit;
+
+        void trigger_collision(Actor *actor)
+        {
+            onHit.broadcast(actor);
+        }
+    };
+
+    class Character : public Actor
+    {
+    public:
+        int health = 10;
+
+        Character() = default;
+    };
+
+    class Bullet
+    {
+    public:
+        std::shared_ptr<PhysicBody> body;
+        Subscriber<void(Actor *)> on_body_hit;
+
+        Bullet() : body(std::make_shared<PhysicBody>()),
+                   on_body_hit(this, &Bullet::on_hit)
+        {
+            body->onHit.bind(on_body_hit);
+        }
+
+        void on_hit(Actor *actor)
+        {
+            Character *character = dynamic_cast<Character *>(actor);
+            if (character != nullptr)
+            {
+                character->health -= 1;
+            }
+        }
+    };
+
+    Character character;
+
+    EXPECT_EQ(character.health, 10);
+
+    Bullet bullet;
+    bullet.body->trigger_collision(&character);
+
+    EXPECT_EQ(character.health, 9);
+
+    bullet.body->trigger_collision(&character);
+    EXPECT_EQ(character.health, 8);
+
+    bullet.on_body_hit.unbind();
+    bullet.body->trigger_collision(&character);
+    EXPECT_EQ(character.health, 8);
+
+}
+
 // Run all the tests
 int main(int argc, char **argv)
 {
