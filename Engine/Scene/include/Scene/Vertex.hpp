94a7d457043476bcdc9d6c6ab956aae0dc35a753
amasson <giantwow3896@gmail.com>
2024-05-01 23:42:17 +0200

    Vertices constructors

--- Engine/Scene/include/Scene/Vertex.hpp
+++ Engine/Scene/include/Scene/Vertex.hpp
@@ -14,11 +14,20 @@ struct Vertex {
 	glm::vec3 tangent;
 	glm::vec3 bitangent;
 	glm::vec2 uv;
+
+	Vertex() = default;
+	Vertex(const glm::vec3 &p, const glm::vec3 &n, const glm::vec3 &t, const glm::vec3 &b, const glm::vec2 &uv);
+	Vertex(const glm::vec3 &p, const glm::vec3 &n, const glm::vec2 &uv);
 };
 
 struct WeightVertex : Vertex {
 	glm::ivec4 boneIDs = glm::ivec4(0);
 	glm::vec4 boneWeights = glm::vec4(0.0f);
+
+	WeightVertex() = default;
+	WeightVertex(const glm::vec3 &p, const glm::vec3 &n, const glm::vec3 &t, const glm::vec3 &b, const glm::vec2 &uv,
+				 const glm::ivec4 &boneIDs, const glm::vec4 &boneWeights);
+	WeightVertex(const glm::vec3 &p, const glm::vec3 &n, const glm::vec2 &uv);
 };
 
 enum class CubeAxis : uint8_t {
