d65cb259a716506d697a574c109549418436ee50
Ghali Boucetta <gboucett@student.42.fr>
2024-05-05 01:57:38 +0200

    reformat cmake code

--- cmake/Helpers.cmake
+++ cmake/Helpers.cmake
@@ -1,27 +1,27 @@
-function(setup_files)
-    if(ARGV)
-        set(LST ${ARGV})
-    else()
-        set(LST include src)
-    endif()
+function (setup_files)
+	if ( ARGV )
+		set(LST ${ARGV})
+	else ()
+		set(LST include src)
+	endif ()
 
-    set(TMP)
+	set(TMP)
 
-    foreach(path IN ITEMS ${LST})
-        file(GLOB_RECURSE LOCAL_SRCS
-            "${path}/*.cpp"
-            "${path}/*.hpp"
-            "${path}/*.cc"
-            "${path}/*.hh"
-            "${path}/*.c"
-            "${path}/*.h"
-        )
+	foreach ( path IN ITEMS ${LST} )
+		file(GLOB_RECURSE LOCAL_SRCS
+				"${path}/*.cpp"
+				"${path}/*.hpp"
+				"${path}/*.cc"
+				"${path}/*.hh"
+				"${path}/*.c"
+				"${path}/*.h"
+		)
 
-        set(TMP ${TMP} ${LOCAL_SRCS})
-    endforeach()
+		set(TMP ${TMP} ${LOCAL_SRCS})
+	endforeach ()
 
-    set(SRCS ${TMP} PARENT_SCOPE)
-endfunction()
+	set(SRCS ${TMP} PARENT_SCOPE)
+endfunction ()
 
 #[[
     function check_dependencies(
@@ -36,41 +36,41 @@ endfunction()
 
     It will set ${module_name}_ALL_FOUND accordingly.
 ]]
-function(check_dependencies module_name)
-    set(FLAGS FATAL_ERROR WARNING)
-    set(MULTI_VALUE_OPTIONS VARIABLE_DEPS TARGET_DEPS)
-    cmake_parse_arguments(PARSE_ARGV 1 CHK_DEPS "${FLAGS}" "" "${MULTI_VALUE_OPTIONS}")
+function (check_dependencies module_name)
+	set(FLAGS FATAL_ERROR WARNING)
+	set(MULTI_VALUE_OPTIONS VARIABLE_DEPS TARGET_DEPS)
+	cmake_parse_arguments(PARSE_ARGV 1 CHK_DEPS "${FLAGS}" "" "${MULTI_VALUE_OPTIONS}")
 
-    set(ALL_FOUND ON)
+	set(ALL_FOUND ON)
 
-    foreach(DEP IN ITEMS ${CHK_DEPS_TARGET_DEPS})
-        message(TRACE "module ${module_name}: checking target dep ${DEP}")
+	foreach ( DEP IN ITEMS ${CHK_DEPS_TARGET_DEPS} )
+		message(TRACE "module ${module_name}: checking target dep ${DEP}")
 
-        if(NOT TARGET ${DEP})
-            message(SEND_ERROR "${DEP} is required to build the ${module_name} module")
-            set(ALL_FOUND OFF)
-        endif()
-    endforeach()
+		if ( NOT TARGET ${DEP} )
+			message(SEND_ERROR "${DEP} is required to build the ${module_name} module")
+			set(ALL_FOUND OFF)
+		endif ()
+	endforeach ()
 
-    foreach(VAR IN LISTS ${CHK_DEPS_VARIABLE_DEPS})
-        message(STATUS "module ${module_name}: checking variable dep ${VAR}")
+	foreach ( VAR IN LISTS ${CHK_DEPS_VARIABLE_DEPS} )
+		message(STATUS "module ${module_name}: checking variable dep ${VAR}")
 
-        if(NOT DEFINED VAR)
-            message(SEND_ERROR "variable ${VAR} is required to build the ${module_name} module")
-            set(ALL_FOUND OFF)
-        endif()
-    endforeach()
+		if ( NOT DEFINED VAR )
+			message(SEND_ERROR "variable ${VAR} is required to build the ${module_name} module")
+			set(ALL_FOUND OFF)
+		endif ()
+	endforeach ()
 
-    if(NOT ALL_FOUND)
-        if(CHK_DEPS_FATAL_ERROR)
-            message(FATAL_ERROR "module ${module_name}: Some dependencies are missing")
-        elseif(CHK_WARNING)
-            message(WARNING "module ${module_name}: Some dependencies are missing")
-        endif()
-    endif()
+	if ( NOT ALL_FOUND )
+		if ( CHK_DEPS_FATAL_ERROR )
+			message(FATAL_ERROR "module ${module_name}: Some dependencies are missing")
+		elseif ( CHK_WARNING )
+			message(WARNING "module ${module_name}: Some dependencies are missing")
+		endif ()
+	endif ()
 
-    set("${module_name}_ALL_FOUND" ${ALL_FOUND} PARENT_SCOPE)
-endfunction()
+	set("${module_name}_ALL_FOUND" ${ALL_FOUND} PARENT_SCOPE)
+endfunction ()
 
 #[[
     function setup_module(
@@ -103,141 +103,141 @@ endfunction()
     pipeline that will generate the documentation to avoid the need of setting
     up all the dependencies (especially Vulkan and GLFW).
 ]]
-function(setup_module)
-    set(FLAGS ENABLE_TESTS FATAL_ERROR WARNING)
-    set(SINGLE_VALUE_OPTIONS NAME)
-    set(MULTI_VALUE_OPTIONS TARGET_DEPS VARIABLE_DEPS SPECIAL_HEADER_PATHS SPECIAL_LIBS)
-    cmake_parse_arguments(PARSE_ARGV 0 SETUP_MODULE "${FLAGS}" "${SINGLE_VALUE_OPTIONS}" "${MULTI_VALUE_OPTIONS}")
+function (setup_module)
+	set(FLAGS ENABLE_TESTS FATAL_ERROR WARNING)
+	set(SINGLE_VALUE_OPTIONS NAME)
+	set(MULTI_VALUE_OPTIONS TARGET_DEPS VARIABLE_DEPS SPECIAL_HEADER_PATHS SPECIAL_LIBS)
+	cmake_parse_arguments(PARSE_ARGV 0 SETUP_MODULE "${FLAGS}" "${SINGLE_VALUE_OPTIONS}" "${MULTI_VALUE_OPTIONS}")
 
-    if(NOT DEFINED SETUP_MODULE_NAME)
-        message(SEND_ERROR "Expecting argument NAME for function setup_module")
-        return()
-    endif()
+	if ( NOT DEFINED SETUP_MODULE_NAME )
+		message(SEND_ERROR "Expecting argument NAME for function setup_module")
+		return()
+	endif ()
 
-    setup_files(src include)
+	setup_files(src include)
 
-    set(SRCS_FOR_DOCS ${SRCS_FOR_DOCS} ${SRCS} CACHE INTERNAL "")
+	set(SRCS_FOR_DOCS ${SRCS_FOR_DOCS} ${SRCS} CACHE INTERNAL "")
 
-    if(NOT FULL_CONFIGURE)
-        message(NOTICE "module ${SETUP_MODULE_NAME}: Won't declare targets")
-        return()
-    endif()
+	if ( NOT FULL_CONFIGURE )
+		message(NOTICE "module ${SETUP_MODULE_NAME}: Won't declare targets")
+		return()
+	endif ()
 
-    if(SETUP_MODULE_FATAL_ERROR)
-        set(DEPS ${DEPS} FATAL_ERROR)
-    elseif(SETUP_MODULE_WARNING)
-        set(DEPS ${DEPS} WARNING)
-    endif()
+	if ( SETUP_MODULE_FATAL_ERROR )
+		set(DEPS ${DEPS} FATAL_ERROR)
+	elseif ( SETUP_MODULE_WARNING )
+		set(DEPS ${DEPS} WARNING)
+	endif ()
 
-    if(SETUP_MODULE_TARGET_DEPS)
-        set(DEPS ${DEPS} TARGET_DEPS ${SETUP_MODULE_TARGET_DEPS})
-    endif()
+	if ( SETUP_MODULE_TARGET_DEPS )
+		set(DEPS ${DEPS} TARGET_DEPS ${SETUP_MODULE_TARGET_DEPS})
+	endif ()
 
-    if(SETUP_MODULE_VARIABLE_DEPS)
-        set(DEPS ${DEPS} VARIABLE_DEPS ${SETUP_MODULE_VARIABLE_DEPS})
-    endif()
+	if ( SETUP_MODULE_VARIABLE_DEPS )
+		set(DEPS ${DEPS} VARIABLE_DEPS ${SETUP_MODULE_VARIABLE_DEPS})
+	endif ()
 
-    check_dependencies(${SETUP_MODULE_NAME} ${DEPS})
-    set(ALL_FOUND "${SETUP_MODULE_NAME}_ALL_FOUND")
+	check_dependencies(${SETUP_MODULE_NAME} ${DEPS})
+	set(ALL_FOUND "${SETUP_MODULE_NAME}_ALL_FOUND")
 
-    if(NOT ${ALL_FOUND})
-        message(SEND_ERROR "module ${SETUP_MODULE_NAME}: Some dependencies are missing")
-        return()
-    else()
-        message(TRACE "module ${SETUP_MODULE_NAME}: got ALL_FOUND = ${ALL_FOUND} -> ${${ALL_FOUND}}")
-    endif()
+	if ( NOT ${ALL_FOUND} )
+		message(SEND_ERROR "module ${SETUP_MODULE_NAME}: Some dependencies are missing")
+		return()
+	else ()
+		message(TRACE "module ${SETUP_MODULE_NAME}: got ALL_FOUND = ${ALL_FOUND} -> ${${ALL_FOUND}}")
+	endif ()
 
-    add_library(${SETUP_MODULE_NAME} ${SRCS})
-    target_include_directories(${SETUP_MODULE_NAME}
-        PUBLIC include
-        PRIVATE src
-        PRIVATE ${PROJECT_BINARY_DIR}/include
-    )
-    target_enable_clang_tidy(${SETUP_MODULE_NAME})
+	add_library(${SETUP_MODULE_NAME} ${SRCS})
+	target_include_directories(${SETUP_MODULE_NAME}
+			PUBLIC include
+			PRIVATE src
+			PRIVATE ${PROJECT_BINARY_DIR}/include
+	)
+	target_enable_clang_tidy(${SETUP_MODULE_NAME})
 
-    set(UNIX_COMPILER_IDS GNU AppleClang Clang)
-    message(STATUS "module ${SETUP_MODULE_NAME}: Enabling warning flags for target")
+	set(UNIX_COMPILER_IDS GNU AppleClang Clang)
+	message(STATUS "module ${SETUP_MODULE_NAME}: Enabling warning flags for target")
 
-    if(CMAKE_CXX_COMPILER_ID IN_LIST UNIX_COMPILER_IDS)
-        message(DEBUG "module ${SETUP_MODULE_NAME}: Enabling -Wall -Wextra")
-        target_compile_options(${SETUP_MODULE_NAME}
-            PUBLIC -Wall
-            PUBLIC -Wextra
-        )
+	if ( CMAKE_CXX_COMPILER_ID IN_LIST UNIX_COMPILER_IDS )
+		message(DEBUG "module ${SETUP_MODULE_NAME}: Enabling -Wall -Wextra")
+		target_compile_options(${SETUP_MODULE_NAME}
+				PUBLIC -Wall
+				PUBLIC -Wextra
+		)
 
-        if(CMAKE_BUILD_TYPE STREQUAL "Release")
-            message(NOTICE "module ${SETUP_MODULE_NAME}: Enabling error flag")
-            message(DEBUG "module ${SETUP_MODULE_NAME}: Enabling flag -Werror")
-            target_compile_options(${SETUP_MODULE_NAME} PRIVATE -Werror)
-        endif()
-    else()
-        message(AUTHOR_WARNING "module ${SETUP_MODULE_NAME}: Current compiler isn't supported for enabling warning or error flags")
-    endif()
+		if ( CMAKE_BUILD_TYPE STREQUAL "Release" )
+			message(NOTICE "module ${SETUP_MODULE_NAME}: Enabling error flag")
+			message(DEBUG "module ${SETUP_MODULE_NAME}: Enabling flag -Werror")
+			target_compile_options(${SETUP_MODULE_NAME} PRIVATE -Werror)
+		endif ()
+	else ()
+		message(AUTHOR_WARNING "module ${SETUP_MODULE_NAME}: Current compiler isn't supported for enabling warning or error flags")
+	endif ()
 
-    if(DEFINED SETUP_MODULE_SPECIAL_HEADER_PATHS)
-        foreach(path IN ITEMS ${SETUP_MODULE_SPECIAL_HEADER_PATHS})
-            target_include_directories(${SETUP_MODULE_NAME} PRIVATE ${path})
-        endforeach()
-    endif()
+	if ( DEFINED SETUP_MODULE_SPECIAL_HEADER_PATHS )
+		foreach ( path IN ITEMS ${SETUP_MODULE_SPECIAL_HEADER_PATHS} )
+			target_include_directories(${SETUP_MODULE_NAME} PRIVATE ${path})
+		endforeach ()
+	endif ()
 
-    if(DEFINED SETUP_MODULE_TARGET_DEPS)
-        foreach(dep IN ITEMS ${SETUP_MODULE_TARGET_DEPS})
-            target_link_libraries(${SETUP_MODULE_NAME} PUBLIC ${dep})
-        endforeach()
-    endif()
+	if ( DEFINED SETUP_MODULE_TARGET_DEPS )
+		foreach ( dep IN ITEMS ${SETUP_MODULE_TARGET_DEPS} )
+			target_link_libraries(${SETUP_MODULE_NAME} PUBLIC ${dep})
+		endforeach ()
+	endif ()
 
-    if(DEFINED SETUP_MODULE_SPECIAL_LIBS)
-        foreach(dep IN ITEMS ${SETUP_MODULE_SPECIAL_LIBS})
-            target_link_libraries(${SETUP_MODULE_NAME} PRIVATE ${dep})
-        endforeach()
-    endif()
+	if ( DEFINED SETUP_MODULE_SPECIAL_LIBS )
+		foreach ( dep IN ITEMS ${SETUP_MODULE_SPECIAL_LIBS} )
+			target_link_libraries(${SETUP_MODULE_NAME} PRIVATE ${dep})
+		endforeach ()
+	endif ()
 
-    if(SETUP_MODULE_ENABLE_TESTS AND STONE_ENGINE_ENABLE_TESTS)
-        if(IS_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/test")
-            enable_testing()
-            set(SRCS "") # Empties old SRCS
-            setup_files(test)
+	if ( SETUP_MODULE_ENABLE_TESTS AND STONE_ENGINE_ENABLE_TESTS )
+		if ( IS_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/test" )
+			enable_testing()
+			set(SRCS "") # Empties old SRCS
+			setup_files(test)
 
-            set(TEST_EXEC "test_${SETUP_MODULE_NAME}")
-            add_executable(${TEST_EXEC} EXCLUDE_FROM_ALL ${SRCS})
-            target_link_libraries(${TEST_EXEC}
-                PRIVATE GTest::gtest_main
-                PRIVATE ${SETUP_MODULE_NAME}
-            )
-            gtest_discover_tests(${TEST_EXEC})
-            message(STATUS "Discovering tests for module ${SETUP_MODULE_NAME} in executable ${TEST_EXEC}")
+			set(TEST_EXEC "test_${SETUP_MODULE_NAME}")
+			add_executable(${TEST_EXEC} EXCLUDE_FROM_ALL ${SRCS})
+			target_link_libraries(${TEST_EXEC}
+					PRIVATE GTest::gtest_main
+					PRIVATE ${SETUP_MODULE_NAME}
+			)
+			gtest_discover_tests(${TEST_EXEC})
+			message(STATUS "Discovering tests for module ${SETUP_MODULE_NAME} in executable ${TEST_EXEC}")
 
-            add_dependencies(build_tests ${TEST_EXEC})
-        else()
-            message(AUTHOR_WARNING "module ${SETUP_MODULE_NAME}: tests was enabled but no directory test was found")
-        endif()
-    endif()
-endfunction()
+			add_dependencies(build_tests ${TEST_EXEC})
+		else ()
+			message(AUTHOR_WARNING "module ${SETUP_MODULE_NAME}: tests was enabled but no directory test was found")
+		endif ()
+	endif ()
+endfunction ()
 
-function(get_all_targets var)
-    set(targets)
-    get_all_targets_recursive(targets ${CMAKE_CURRENT_LIST_DIR})
-    set(${var} ${targets} PARENT_SCOPE)
-endfunction()
+function (get_all_targets var)
+	set(targets)
+	get_all_targets_recursive(targets ${CMAKE_CURRENT_LIST_DIR})
+	set(${var} ${targets} PARENT_SCOPE)
+endfunction ()
 
-macro(get_all_targets_recursive targets dir)
-    get_property(subdirectories DIRECTORY ${dir} PROPERTY SUBDIRECTORIES)
+macro (get_all_targets_recursive targets dir)
+	get_property(subdirectories DIRECTORY ${dir} PROPERTY SUBDIRECTORIES)
 
-    foreach(subdir IN ITEMS ${subdirectories})
-        get_all_targets_recursive(${targets} ${subdir})
-    endforeach()
+	foreach ( subdir IN ITEMS ${subdirectories} )
+		get_all_targets_recursive(${targets} ${subdir})
+	endforeach ()
 
-    get_property(current_targets DIRECTORY ${dir} PROPERTY BUILDSYSTEM_TARGETS)
-    list(APPEND ${targets} ${current_targets})
-endmacro()
+	get_property(current_targets DIRECTORY ${dir} PROPERTY BUILDSYSTEM_TARGETS)
+	list(APPEND ${targets} ${current_targets})
+endmacro ()
 
-macro(get_subdirs result current_dir)
-    file(GLOB children RELATIVE ${current_dir} ${current_dir}/*)
-    set(dir_list "")
-    foreach (child ${children})
-        if (IS_DIRECTORY ${current_dir}/${child})
-            list(APPEND dir_list ${child})
-        endif ()
-    endforeach ()
-    set(${result} ${dir_list})
-endmacro()
+macro (get_subdirs result current_dir)
+	file(GLOB children RELATIVE ${current_dir} ${current_dir}/*)
+	set(dir_list "")
+	foreach ( child ${children} )
+		if ( IS_DIRECTORY ${current_dir}/${child} )
+			list(APPEND dir_list ${child})
+		endif ()
+	endforeach ()
+	set(${result} ${dir_list})
+endmacro ()
