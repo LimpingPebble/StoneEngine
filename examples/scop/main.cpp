6cb443b4aa105f6ac791af7f056a50f6681c1612
amasson <giantwow3896@gmail.com>
2024-05-17 00:11:03 +0200

    Scop example with shader

--- examples/scop/main.cpp
+++ examples/scop/main.cpp
@@ -36,14 +36,15 @@ int main() {
 
 	int retCode;
 	{
+		// Create an App
 		auto app = std::make_shared<Stone::Window::App>();
 
+		// Create a Window
 		Stone::Window::WindowSettings win_settings;
 		win_settings.title = "Scop";
 		auto window = app->createWindow(win_settings);
 
-		auto meshNode = window->getWorld()->addChild<Stone::Scene::MeshNode>();
-
+		// Generate a Mesh
 		auto mesh = std::make_shared<Stone::Scene::Mesh>();
 		mesh->indicesRef() = {0, 1, 2, 0, 2, 3};
 		mesh->verticesRef().emplace_back();
@@ -59,32 +60,46 @@ int main() {
 		mesh->verticesRef().back().position = {-0.5f, 0.5f, 0.0f};
 		mesh->verticesRef().back().uv = {0.0f, 1.0f};
 
+		// Create a MeshNode
+		auto meshNode = window->getWorld()->addChild<Stone::Scene::MeshNode>();
 		meshNode->setMesh(mesh);
 
+		// Create a Texture
+		auto stone_texture = std::make_shared<Stone::Scene::Texture>();
 		auto stone_image_source =
 			std::make_shared<Stone::Image::ImageSource>("docs/img/stone-engine.png", Stone::Image::Channel::RGBA);
-
-		auto stone_texture = std::make_shared<Stone::Scene::Texture>();
 		stone_texture->setImage(stone_image_source);
 
+		// Create a Material using the texture
 		auto stone_material = std::make_shared<Stone::Scene::Material>();
 		stone_material->setTextureParameter("diffuse", stone_texture);
 		meshNode->setMaterial(stone_material);
 
+		// Create a shader from a file
+		auto stone_shader = std::make_shared<Stone::Scene::Shader>("shaders/frag.spv");
+		stone_material->setFragmentShader(stone_shader);
+
+		// Create a second MeshNode with the same mesh
 		auto meshRotatingNode = window->getWorld()->addChild<RotatingNode>();
 		meshRotatingNode->getTransform().setPosition({0.0f, 0.0f, 0.0f});
 		auto secondMeshNode = meshRotatingNode->addChild<Stone::Scene::MeshNode>();
 		meshRotatingNode->setRotationSpeed({0.0f, 0.4f, 0.0f});
 		secondMeshNode->setMesh(mesh);
-		secondMeshNode->setMaterial(stone_material);
 
-		auto rotatingNode = window->getWorld()->addChild<RotatingNode>();
-		auto cameraNode = rotatingNode->addChild<Stone::Scene::PerspectiveCameraNode>();
+		// Create a blue material that takes no texture as everything is in the shader code
+		auto blueShader = std::make_shared<Stone::Scene::Shader>("shaders/frag-blue.glsl");
+		auto blueMaterial = std::make_shared<Stone::Scene::Material>();
+		blueMaterial->setFragmentShader(blueShader);
+		secondMeshNode->setMaterial(stone_material /* blueMaterial */);
+
+		// Create a camera moving around the scene
+		auto cameraRotator = window->getWorld()->addChild<RotatingNode>();
+		auto cameraNode = cameraRotator->addChild<Stone::Scene::PerspectiveCameraNode>();
 		cameraNode->getTransform().setPosition({0.0f, 3.0f, 3.0f});
 		cameraNode->getTransform().rotate({-0.6f, 0.0f, 0.0f});
-
 		window->getWorld()->setActiveCamera(cameraNode);
 
+		// Run the App
 		retCode = app->run();
 	}
 
--- shaders/vert.glsl
+++ shaders/vert.glsl
@@ -12,11 +12,9 @@ layout(location = 2) in vec3 tangent;
 layout(location = 3) in vec3 bitangent;
 layout(location = 4) in vec2 uv;
 
-layout(location = 0) out vec3 fragColor;
-layout(location = 1) out vec2 fragUV;
+layout(location = 0) out vec2 fragUV;
 
 void main() {
     gl_Position = ubo.proj * ubo.view * ubo.model * vec4(position, 1.0);
-    fragColor = vec3(uv, 1.0);
     fragUV = uv;
 }
