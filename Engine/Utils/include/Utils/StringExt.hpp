15ab19a56b66c29e2743e1de820ef72cd3c33918
amasson <giantwow3896@gmail.com>
2024-05-28 14:52:13 +0200

    string utils functions

---
+++ Engine/Utils/include/Utils/StringExt.hpp
@@ -0,0 +1,9 @@
+// Copyright 2024 Stone-Engine
+
+#pragma once
+
+#include <string>
+
+bool string_ends_with(const std::string &str, const std::string &suffix);
+bool string_starts_with(const std::string &str, const std::string &prefix);
+bool string_contains_non_printable(const std::string &str);
