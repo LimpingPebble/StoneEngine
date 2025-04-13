bbc07a3674b682300e9e7f79d68f27df1ffadbee
amasson <arthur.masson.3896@gmail.com>
2025-04-13 12:49:52 +0200

    fix format

--- Engine/Scene/src/Scene/Shader/ShaderParameters.cpp
+++ Engine/Scene/src/Scene/Shader/ShaderParameters.cpp
@@ -14,10 +14,13 @@ void ShaderParameters::setParamWithName(const std::string &name, Type value) {
 	using ParamSetter = std::function<void(ShaderParameters &, Type)>;
 
 #define __MAP_NAME_TO_PARAM(param)                                                                                     \
-	{#param, [](ShaderParameters &matParams, Type value) {                                                             \
-		 matParams.param = value;                                                                                      \
-	 }},
-
+	{                                                                                                                  \
+		#param, [](ShaderParameters &matParams, Type value) {                                                          \
+			matParams.param = value;                                                                                   \
+		}                                                                                                              \
+	}                                                                                                                  \
+	,
+ 
 	const static std::unordered_map<std::string, ParamSetter> paramSetters = {
 		FOR_EACH_SHADER_PARAMETERS(__MAP_NAME_TO_PARAM) //
 	};
