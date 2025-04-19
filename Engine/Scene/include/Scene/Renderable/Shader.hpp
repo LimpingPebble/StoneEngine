b725c33c44a84eb12d68ebade8663a22f3e5e2d1
amasson <arthur.masson.3896@gmail.com>
2025-04-19 23:56:30 +0200

    custom fragment shader take inout Material

--- Engine/Scene/include/Scene/Renderable/Shader.hpp
+++ Engine/Scene/include/Scene/Renderable/Shader.hpp
@@ -86,9 +86,9 @@ public:
 	void setLocation(const std::string &name, int location);
 
 private:
-	ContentType _contentType = ContentType::SourceCode; /** The type of the content. */
-	std::string _content = "void customShader() {}";	/** The content of the shader. */
-	std::string _function = "customShader";				/** The function to call in the shader. */
+	ContentType _contentType = ContentType::SourceCode;					   /** The type of the content. */
+	std::string _content = "void customShader(inout Material fragMat) {}"; /** The content of the shader. */
+	std::string _function = "customShader";								   /** The function to call in the shader. */
 
 	std::unordered_map<std::string, int> _locations = {}; /** The binding locations of the variables in the shader. */
 	int _maxLocation = -1;								  /** The cached maximum value from the locations. */
